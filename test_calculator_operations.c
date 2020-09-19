fd0ece79a67f438666f3d77df835f15c9edd3593
vijaymksv <48716574+vijaymksv@users.noreply.github.com>
2020-09-19 07:35:19 +0530

    Add files via upload

---
+++ test_calculator_operations.c
@@ -0,0 +1,91 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <calculator_operations.h>
+#define PROJECT_NAME    "Calculator"
+
+/* Prototypes for all the test functions */
+void test_add(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
+void test_modulus(void);
+void test_greater(void);
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "add", test_add);
+  CU_add_test(suite, "subtract", test_subtract);
+  CU_add_test(suite, "multiply", test_multiply);
+  CU_add_test(suite, "divide", test_divide);
+  CU_add_test(suite, "modulus", test_modulus);
+  CU_add_test(suite, "greater", test_modulus);
+  
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+void test_add(void) {
+  CU_ASSERT(30 == add(10, 20));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1500 == add(750, 7500));
+}
+
+void test_subtract(void) {
+  CU_ASSERT(-3 == subtract(0, 3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == subtract(1000, 900));
+}
+
+void test_multiply(void) {
+  CU_ASSERT(0 == multiply(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == multiply(2, 5));
+}
+
+void test_divide(void) {
+  CU_ASSERT(0 == divide(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == divide(2, 2));
+}
+
+void test_modulus(void) {
+  CU_ASSERT(0 == modulus(1, 0));
+  CU_ASSERT(1 == modulus(3, 2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == modulus(2, 2));
+}
+
+void test_greater(void) {
+  CU_ASSERT(3 == greater(3, 2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == greater(20, 2));
+}
